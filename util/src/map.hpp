776158cef310805cc537c13372fa2aa5ea093304
liuyulvv <liuyulvv@outlook.com>
2022-07-12 22:19:29 +0800

    线程安全map

---
+++ util/src/map.hpp
@@ -0,0 +1,37 @@
+/**
+ * @file map.hpp
+ * @author liuyulvv (liuyulvv@outlook.com)
+ * @version 0.1
+ * @date 2022-07-11
+ */
+
+#pragma once
+
+#include <map>
+#include <mutex>
+
+namespace util {
+
+template <typename T1, typename T2>
+class Map final {
+public:
+    Map() = default;
+    ~Map() = default;
+
+public:
+    void Insert(const T1 &key, const T2 &value) {
+        std::lock_guard<std::mutex> locker(mutex_);
+        map_[key] = value;
+    }
+
+    void Erase(const T1 &key) {
+        std::lock_guard<std::mutex> locker(mutex_);
+        map_.erase(key);
+    }
+
+private:
+    std::map<T1, T2> map_;
+    std::mutex mutex_;
+};
+
+} // namespace util
