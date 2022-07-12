2b13c1c7a4a38ebfd52e5027928e302a92b768c5
liuyulvv <liuyulvv@outlook.com>
2022-07-12 22:24:32 +0800

    整形别称

---
+++ util/src/alias.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file alias.hpp
+ * @author liuyulvv (liuyulvv@outlook.com)
+ * @version 0.1
+ * @date 2022-07-12
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace util {
+    using Int8 = int8_t;
+    using Int16 = int16_t;
+    using Int32 = int32_t;
+    using Int64 = int64_t;
+    using UInt8 = uint8_t;
+    using UInt16 = uint16_t;
+    using UInt32 = uint32_t;
+    using UInt64 = uint64_t;
+}
